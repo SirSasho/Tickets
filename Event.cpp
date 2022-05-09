b2c15cf1f2f5b31c34f606dba7bf11fcb1742de8
Alexander Kunev <73391613+SirSasho@users.noreply.github.com>
2022-05-09 22:52:57 +0300

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -4,7 +4,7 @@ void Event::copyFrom(const Event& other)
 {
     date = other.date;
     hall = other.hall;
-    strcpy(this->name, other.name);
+    name = other.name;
 
     size_t rows = hall.getRows();
     size_t cols = hall.getSeatsByRow();
@@ -15,7 +15,6 @@ void Event::copyFrom(const Event& other)
             seats[i][j] = other.seats[i][j];
         }
     }
-
 }
 
 void Event::free()
@@ -26,26 +25,20 @@ void Event::free()
         delete seats[i];
     }
     delete[] seats;
-    delete[] name;
+    
+    name.clear();            //delete[] name;
 }
 
-Event::Event(const char* name, Date& date, Hall& hall)
-    :name(nullptr), date(date), hall(hall), seats(nullptr)
+Event::Event(const std::string name, Date& date, Hall& hall)
+    :name(""), date(date), hall(hall), seats(nullptr)
 {
     size_t rows = hall.getRows();
     size_t cols = hall.getSeatsByRow();
-    setName(name);
-    seats = new char* [rows];
+    this->name = name;//setName(name)
+    seats = new Seats * [rows];
     for (size_t i = 0; i < rows; i++)
     {
-        seats[i] = new char[cols];
-    }
-    for (size_t i = 0; i < rows; i++)
-    {
-        for (size_t j = 0; j < cols; j++)
-        {
-            seats[i][j] = '2';
-        }
+        seats[i] = new Seats[cols];
     }
 }
 
@@ -59,7 +52,7 @@ Event& Event::operator=(const Event& other)
 {
     if (this != &other)
     {
-        //delete[] name;
+        free();
         copyFrom(other);
     }
     return *this;
@@ -69,25 +62,30 @@ Event::~Event()
 {
     free();
 }
-
+/*
 void Event::setName(const char* name)
 {
     delete[] this->name;
     this->name = new char[strlen(name) + 1];
     strcpy(this->name, name);
 }
-
+*/
 const Date& Event::getDate() const
 {
     return date;
 }
 
-unsigned Event::getHall() const
+unsigned Event::getHallId() const
 {
     return hall.getId();
 }
 
-const char* Event::getName()
+Hall& Event::getHall()
+{
+    return hall;
+}
+
+const std::string Event::getName()
 {
     return name;
 }
@@ -101,18 +99,27 @@ size_t Event::getFreeSeats() const
     {
         for (size_t j = 0; j < cols; j++)
         {
-            if (seats[i][j] == '2')
+            if (seats[i][j].seatType == seatTypes::empty)
                 ++cnt;
         }
     }
     return cnt;
 }
 
-char Event::getSeat(size_t row, size_t col)
+Seats Event::getSeat(size_t row, size_t col)
 {
-    //правя нов чар* и го присвоявам и връщам него
-    //char *temp = &seats[row][col];
-    return &seats[row][col];
+    return seats[row][col];
+}
+
+seatTypes Event::getSeatType(size_t row, size_t col)
+{
+    return seats[row][col].seatType;
+}
+
+void Event::updateSeat(size_t row, size_t col, std::string password, seatTypes seatType)
+{
+    this->seats[row][col].password = password;
+    this->seats[row][col].seatType = seatType;
 }
 
 void Event::print() const
