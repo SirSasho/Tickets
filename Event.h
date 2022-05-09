d6ffb4c7e529cc5759261a66f0c6cc1c216de0c0
Alexander Kunev <73391613+SirSasho@users.noreply.github.com>
2022-05-09 22:53:24 +0300

    Update Event.h

--- Event.h
+++ Event.h
@@ -1,30 +1,37 @@
 #pragma once
 #include "Hall.h"
 #include "Date.h"
+#include "Seats.h"
+#include <cstring>
 class Event
 {
 private:
 	Date& date;
-	char* name;
+	std::string name;
 	Hall& hall;
-	char** seats; /// 2-free, 1-reserve, 0-sold //
+	Seats** seats;/// 0-free, 1-reserve, 2-sold //
 
 	void copyFrom(const Event& other);
 	void free();
 
 public:
-	Event(const char* name, Date& date, Hall& hall);
+	
+	Event(const std::string name, Date& date, Hall& hall);
 	//Event(const char* name, size_t day, size_t month, size_t year, unsigned id, size_t rows, size_t seatsByRows);
 	Event& operator=(const Event& other);
 	~Event();
 
 
-	void setName(const  char* name);
+	//void setName(const char* name);
 	const Date& getDate() const;
-	unsigned getHall() const;
-	const char* getName();
+	unsigned getHallId() const;
+	Hall& getHall();
+	const std::string getName();
 	size_t getFreeSeats() const;
-	char  getSeat(size_t row, size_t col);
+	Seats getSeat(size_t row, size_t col);
+	seatTypes getSeatType(size_t row, size_t col);
+
+	void updateSeat(size_t row, size_t col, std::string password, seatTypes seatType);
 
 
 	void print() const;
