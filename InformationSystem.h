515dc3bd7430ab08f0e2c7d29946cc2ae9c48ed1
Alexander Kunev <73391613+SirSasho@users.noreply.github.com>
2022-05-11 23:47:17 +0300

    Update InformationSystem.h

--- InformationSystem.h
+++ InformationSystem.h
@@ -2,32 +2,37 @@
 #include "Event.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 class InformationSystem
 {
 private:
-	Event** events;
-	size_t capacity;
+	std::vector<Event*> events;
+	//Event** events;
+	//size_t capacity;
 	size_t cnt;
 
-	void copyFrom(const InformationSystem& other);
+	//void copyFrom(const InformationSystem& other);
 	void free();
-	void reallocate();
+	//void reallocate();
 
 public:
 	InformationSystem();
-	InformationSystem(const InformationSystem& other);
-	InformationSystem& operator=(const InformationSystem& other);
+	//InformationSystem(const InformationSystem& other);
+	//InformationSystem& operator=(const InformationSystem& other);
 	~InformationSystem();
 
 	size_t getCnt() const;
-	size_t getCapacity() const;
-	void pushEvent(Event& event);
+	//size_t getCapacity() const;
+	void pushEvent(Event* event);
 	void freeSeats(const std::string name, Date& date);
 	void reserveTickets(const std::string, Date& date, size_t row, size_t col, std::string password);
 	void unReserveTickets(const std::string, Date& date, size_t row, size_t col, std::string password);
 	void buyTickets(const std::string, Date& date, size_t row, size_t col);
 	std::ostream& listWithReservetion(std::string name, Date& date);
+	void helperList(std::ostream& file, size_t k);
 	std::ostream& reportForSoldTickets(Hall& hall, Date& date, Date& date1);
+	void helperReport(std::ostream& file, size_t rows, size_t cols, size_t e);
+	
 
 	void print(std::ostream& os = std::cout) const;
 
