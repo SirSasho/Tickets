98d4c61a3338db44c1fa6a87bc3e6d3ff17d4c60
Alexander Kunev <73391613+SirSasho@users.noreply.github.com>
2022-05-15 20:30:43 +0300

    Update Date.h

--- Date.h
+++ Date.h
@@ -10,14 +10,13 @@ private:
 public:
 	Date(size_t day, size_t month, size_t year);
 	
-
 	size_t getYear() const;
 	size_t getMonth() const;
 	size_t getDay() const;
-
 	void print() const;
 };
 
 std::ostream& operator<<(std::ostream&, const Date&);
 bool operator==(const Date& lhs, const Date& rhs);
+
 bool operator>(const Date& lhs, const Date& rhs);
